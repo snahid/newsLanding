5d68677df66589efbf21fa5b137009139d3ddf4a
snahid <nahidchawdhury77@gmail.com>
2021-12-20 14:33:12 +0600

    add pop up

--- js/custom.js
+++ js/custom.js
@@ -120,3 +120,13 @@ $('.cross').click(function () {
     function goBack() {
       window.history.back();
     }
+
+   
+
+
+//   var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
